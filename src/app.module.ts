f830884550bdf7836787f76b61a21a840175ca8e
lee minseop <lms990108@gmail.com>
2024-04-25 13:28:43 +0900

    Fix: 경로 오류 해결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { QuestionsModule } from './domain/questions/questions.module';
+import { UploadsModule } from './domain/uploads/uploads.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UploadsModule,
     QuestionsModule,
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
--- src/domain/questions/questions.module.ts
+++ src/domain/questions/questions.module.ts
@@ -3,12 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
 import { QuestionSchema } from './schemas/question.schema';
-import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Question', schema: QuestionSchema }]),
-    UploadsModule,
   ],
   controllers: [QuestionsController],
   providers: [QuestionsService],
